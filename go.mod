73d25d5aba1b842a45ee70b56e3b131d404488c6
Sagleft <sagleft@inbox.ru>
2023-09-29 10:17:16 +0300

    save license to file

--- go.mod
+++ go.mod
@@ -2,4 +2,16 @@ module github.com/Sagleft/freemium-engine
 
 go 1.19
 
-require github.com/hyperboloide/lk v0.0.0-20230325114855-ce3fecd34798 // indirect
+require (
+	github.com/Sagleft/swiss-knife v1.6.0
+	github.com/hyperboloide/lk v0.0.0-20230325114855-ce3fecd34798
+)
+
+require (
+	github.com/common-nighthawk/go-figure v0.0.0-20210622060536-734e95fb86be // indirect
+	github.com/fatih/color v1.14.1 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.17 // indirect
+	github.com/sagleft/simple-cron v1.4.1 // indirect
+	golang.org/x/sys v0.3.0 // indirect
+)
--- internal/pkg/license/license.go
+++ internal/pkg/license/license.go
@@ -3,15 +3,36 @@ package license
 import (
 	"fmt"
 
+	swissknife "github.com/Sagleft/swiss-knife"
 	"github.com/hyperboloide/lk"
 )
 
 type License interface {
 	Encode() (string, error)
+	Save(filepath string) error
+	Load(filepath string) (License, error)
 }
 
 type defaultLicense struct {
 	licenseData *lk.License
+}
+
+func (l *defaultLicense) Save(filepath string) error {
+	licData, err := l.Encode()
+	if err != nil {
+		return fmt.Errorf("encode: %w", err)
+	}
+
+	if err := swissknife.SaveStringToFile(filepath, licData); err != nil {
+		return fmt.Errorf("save: %w", err)
+	}
+	return nil
+}
+
+func (l *defaultLicense) Load(filepath string) (License, error) {
+	//lk.LicenseFromB32String()
+
+	return nil, nil // TODO
 }
 
 func (l *defaultLicense) Encode() (string, error) {
